9fbc87ad2382669ea93a39f03fa18925dd09dad0
Mustaf <mustafdiiriye@hotmail.com>
2023-10-01 14:33:52 -0500

    Intro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar';
 import './App.css'
+import Intro from './components/Intro';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am</span>
+          <span> Mustaf Abtidon</span>
+          <span>Front Developer with high experience in web designing and mobile applications 
+            and development, producting the quality work.</span>
+        </div>
+         <button className="i-button">Hire Me</button>
+      </div>
+      <div className="i-right"></div>
+    </div>
+  )
+}
+
+export default Intro
