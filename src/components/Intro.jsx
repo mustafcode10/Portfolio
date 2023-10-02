24a2ee84b865234bcfe99a94586b6f252a10b9a7
Mustaf <mustafdiiriye@hotmail.com>
2023-10-01 23:06:21 -0500

    intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import './Intro.css'
 
 function Intro() {
   return (
     <div className="intro">
       <div className="i-left">
         <div className="i-name">
-          <span>Hy! I Am</span>
-          <span> Mustaf Abtidon</span>
+          <span>Hey! I Am</span>
+          <span> Mustaf Abtidon </span>
           <span>Front Developer with high experience in web designing and mobile applications 
             and development, producting the quality work.</span>
         </div>
-         <button className="i-button">Hire Me</button>
+         <button className="button i-button">Hire Me</button>
       </div>
-      <div className="i-right"></div>
+      <div className="i-right">
+        i am right side
+      </div>
     </div>
   )
 }
