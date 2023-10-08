7d39d1427c7aa752d34f8c001d75d32e3712ad02
Mustaf <mustafdiiriye@hotmail.com>
2023-10-08 13:56:02 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import './App.css'
-import Intro from './components/Intro';
+import Intro from './components/Intro/Intro';
+
 function App() {
   return (
     <div className="App">
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Intro.css'
-import Github from './../img/github.png'
-import LinkedIn from './../img/linkedin.png'
-import Instagram from './../img/instagram.png'
+import Github from './../../img/github.png'
+import LinkedIn from './../../img/linkedin.png'
+import Instagram from './../../img/instagram.png'
+import Vector1 from './../../img/Vector1.png'
+import Vector2 from './../../img/Vector2.png'
+import boy from './../../img/boy.png'
+// import thumbup from './../img/thumbup.png'
+// import Crown from './../img/crown.png'
+
 
 
 function Intro() {
@@ -19,8 +25,8 @@ function Intro() {
             As a highly experienced Front-End Developer, I specialize in mobile app development and web design.
             With a passion for creating exceptional user experiences,
             I combine creativity and technical expertise to deliver quality work that not only meets but exceeds expectations.
-             {/* Explore my portfolio to see how I bring innovation and precision to every project. */}
             </span>
+
         </div>
          <button className="button i-button">Hire Me</button>
           <div className="i-icons">
@@ -37,7 +43,9 @@ function Intro() {
           </div>
       </div>
       <div className="i-right">
-        i am right side
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
       </div>
     </div>
   )
